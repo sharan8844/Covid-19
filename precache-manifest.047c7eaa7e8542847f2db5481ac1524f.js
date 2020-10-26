93c3d9f36d905c5b2ab258c2f271282df24f8cb2
sharan8844 <sharanharsh8844@gmail.com>
2020-10-26 20:31:10 +0530

    Updates

---
+++ precache-manifest.047c7eaa7e8542847f2db5481ac1524f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfd602969860fb0c086537becaf8c44",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "b9ec41949be948c82b5b",
+    "url": "/Covid-19/static/css/main.13ae48db.chunk.css"
+  },
+  {
+    "revision": "15113ebf7fd352facf07",
+    "url": "/Covid-19/static/js/2.8d0c20d9.chunk.js"
+  },
+  {
+    "revision": "cd6e711b9e263c2324169127d95e8bd1",
+    "url": "/Covid-19/static/js/2.8d0c20d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ec41949be948c82b5b",
+    "url": "/Covid-19/static/js/main.1e80da39.chunk.js"
+  },
+  {
+    "revision": "9a5a106ce4787a5d28da",
+    "url": "/Covid-19/static/js/runtime-main.f65ba56e.js"
+  }
+]);
